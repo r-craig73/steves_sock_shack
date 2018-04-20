e903461adfa02caf02de68add632acd0248dd841
Ron Craig <ron.craig@comcast.net>
2018-04-20 12:40:48 -0700

    fails product model spec for relationship with review class

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
-  #it { should have_many :reviews }
+  it { should have_many :reviews }
 end
