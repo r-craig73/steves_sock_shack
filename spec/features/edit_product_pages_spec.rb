efd885e22faa3132f5796321845a511e203ed7a0
Ron Craig <ron.craig@comcast.net>
2018-04-22 06:08:40 -0700

    pass edit product page integration test (11 out of 11 passing)

--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -6,7 +6,7 @@ describe "editing a product process" do
     review = Review.create(:author => 'Roxxxy Andrews', :content_body => 'It is a hug, Michael. I am hugging you. I like being with you. Not tricks, Michael, illusions', :rating => 3, :product_id => product.id)
     visit edit_product_url(product)
     fill_in 'Sock name', :with => 'Orange sock'
-    # click_on 'Add Sock'
+    click_on 'Add Sock'
     expect(page).to have_content 'Your sock is updated'
   end
 
