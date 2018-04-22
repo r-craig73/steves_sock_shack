f15bd9e66338ce3dedde02201c886e70d8c32820
Ron Craig <ron.craig@comcast.net>
2018-04-21 23:48:01 -0700

    pass product content_body character limits (7 out of 7 specs passing)

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :product
 
   validates :author, :presence => true
-  validates_length_of :content_body, minimum: 50, maximum: 250, too_short: 'please enter at least 50 characters', too_long: 'please enter less than 250 characters'
+  validates_length_of :content_body, minimum: 50, maximum: 250, too_short: 'is too short (minimum is 50 characters)', too_long: 'is too long (maximum is 250 characters)'
   validates :rating, :presence => true
 
 end
