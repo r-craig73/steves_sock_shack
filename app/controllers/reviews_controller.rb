ae1c307e76051623e11787df8188990d29e2477a
Ron Craig <ron.craig@comcast.net>
2018-04-21 19:56:04 -0700

    adding review routes, views and form...and crashing hard

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,7 +6,30 @@ class ReviewsController < ApplicationController
     render :index
   end
 
-  
+  def new
+    @product = Product.find(params[:product_id])
+    review_id = @product.review_id
+    @review = Review.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    review_id = @product.review_id
+
+    @review = Review.new(review_params)
+    @review.product_id = @product.id
+    if @review.save
+      flash[:notice] = "Thank you for your review. Your review is added to the product."
+      redirect_to product_path(@product.id)
+    else
+      render :new
+    end
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:content_body, :rating)
+  end
 
 
 
