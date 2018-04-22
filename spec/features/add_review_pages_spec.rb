f0a7eee3960ac3fdf2063671a268f7a751886e77
Ron Craig <ron.craig@comcast.net>
2018-04-22 06:11:37 -0700

    fail add review integration test (11 out of 12 passing)

--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 describe "the review process" do
-  # it "adds a new review" do
-  #   product = Product.create(:name => 'Blue Sock', :cost => '4.99', :country_of_origin => 'China', :featured => false)
-  #   review = Review.create(:author => 'Roxxxy Andrews', :content_body => 'It is a hug, Michael. I am hugging you. I like being with you. Not tricks, Michael, illusions', :rating => 3, :product_id => product.id)
-  #   visit product_url(product)
-  #   click_on 'Add a Review'
-  #   fill_in 'Author', :with => 'Bob K.'
-  #   fill_in 'Review', :with => 'xxxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxx'
-  #   select 4, from: 'Rating'
-  #   click_on 'Add Review'
-  #   expect(page).to have_content 'Thank you for your review.'
-  # end
+  it "adds a new review" do
+    product = Product.create(:name => 'Blue Sock', :cost => '4.99', :country_of_origin => 'China', :featured => false)
+    review = Review.create(:author => 'Roxxxy Andrews', :content_body => 'It is a hug, Michael. I am hugging you. I like being with you. Not tricks, Michael, illusions', :rating => 3, :product_id => product.id)
+    visit product_url(product)
+    click_on 'Add a Review'
+    fill_in 'Author', :with => 'Bob K.'
+    # fill_in 'Review', :with => 'xxxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxx xxxxxxxxxxxx'
+    # select 4, from: 'Rating'
+    click_on 'Add Review'
+    expect(page).to have_content 'Thank you for your review.'
+  end
 
   # it "reports an error when name is empty" do
   #   visit new_product_review_path
