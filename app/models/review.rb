6e965b9a5b9c2bb321b64d99ae4a2de53cbcd05e
Ron Craig <ron.craig@comcast.net>
2018-04-21 19:37:54 -0700

    add review content_body lower and upper character limits

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   validates :author, :presence => true
-  validates :content_body, :presence => true
+  validates_length_of :content_body, minimum: 50, maximum: 250, too_short: 'please enter at least 50 characters', too_long: 'please enter less than 250 characters'
+  validates :rating, :presence => true
 
 end
